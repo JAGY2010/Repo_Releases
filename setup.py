b7fcf87aaefbc64dbbf842e03af050a4d1fed60c
Julian Gaviria <juliangaviria29@gmail.com>
2026-07-23 08:50:33 -0400

    Nuevo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Repo_Releases",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Julian Gaviria",                        # Tu nombre
+    author_email="juliangaviria29@gmail.com",       # Tu correo electrónico
+    url="https://github.com/JAGY2010/Repo_Releases",# URL del proyecto
+)
